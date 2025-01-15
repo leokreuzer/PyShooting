4051909118fb22b954bd1206acb2e21c41bf3b97
Leo Kreuzer <leo@familiekreuzer.de>
2025-01-15 11:09:15 +0100

    Update main.py

--- examples/Euler-Bernoulli beam with dry-friction/main.py
+++ examples/Euler-Bernoulli beam with dry-friction/main.py
@@ -1,3 +1,25 @@
+"""   
+    PyShooting is a program that uses the Shooting Method for Periodic Steady-state
+    Nonlinear Structural Dynamics. It also performs numerical continuation to build the frequency response, as well as nonlinear normal modes.
+
+    Copyright (C) 2025  Leo Kreuzer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+
 import numpy as np
 from dual_numbers_dynamics import Dual
 from Mechanical_System_class import mechanical_system_class
