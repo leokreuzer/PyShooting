177e45f25267a7e353361fdde1ee72f08e3993d2
Leo Kreuzer <leo@familiekreuzer.de>
2025-01-15 11:11:28 +0100

    Update solver.py

--- examples/Duffing/solver.py
+++ examples/Duffing/solver.py
@@ -1,3 +1,26 @@
+"""   
+    This file describes the Newton-Raphson solver. 
+    It defines the two distinct types of solvers, one for finding a inital solution and one for finding a solution in the continuation.
+    
+
+    Copyright (C) 2025  Leo Kreuzer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+
 import numpy as np
 import T_integrator
 
