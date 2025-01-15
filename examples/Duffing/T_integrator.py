ee92a4f27122a91b27fc3a8636dcd77f2fe83182
Leo Kreuzer <leo@familiekreuzer.de>
2025-01-15 11:07:31 +0100

    Update T_integrator.py

--- examples/Duffing/T_integrator.py
+++ examples/Duffing/T_integrator.py
@@ -1,3 +1,25 @@
+"""   
+    This file describes the time integration functions. 
+    It defines all implemented time integration methods for natively first order systems.    
+
+    Copyright (C) 2025  Leo Kreuzer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+
 import numpy as np
 from scipy.linalg import inv
 from Mechanical_System_class import mechanical_system_class
